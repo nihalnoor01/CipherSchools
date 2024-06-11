15ad4290d20b46f47d55c9b29848bccfcfc762a4
Nihal.k <121479386+nihalnoor01@users.noreply.github.com>
2024-06-11 11:08:43 +0530

    Add files via upload

---
+++ Lecture4-CipherSchools.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5,b=4;
+    cout<<a+b<<endl;//This will print the sum of a and b
+    cout<<"a+b";//This will print a+b as it is(as a string).
+    //cout<<a+b<<endl<<"a+b"; //This will also gove the same output as above
+}
