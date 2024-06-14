ca486b08a4032ee67fb78f1f5f5fb100bb83b761
Nihal.k <121479386+nihalnoor01@users.noreply.github.com>
2024-06-14 23:48:51 +0530

    Update Lecture7-CipherSchools.cpp

--- Lecture7-CipherSchools.cpp
+++ Lecture7-CipherSchools.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
+    //Traditional Way
+    int a,b,c,d,e;
+    a=1;b=44;c=78;d=54;e=97;
+
+    cout<<a<<b<<c<<d<<e;
+
+    //Declaring an array
     int arr[5]={34,321,1,12,99};
-    cout<<arr[0]<<arr[1]<<arr[2]<<arr[3]<<arr[4];
-    return 0;
+    
     }
