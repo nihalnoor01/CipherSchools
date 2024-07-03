ecf0a6982981a4e333fcd38f200395d70c744a8b
Nihal.k <121479386+nihalnoor01@users.noreply.github.com>
2024-07-03 12:49:39 +0530

    Add files via upload

---
+++ Lecture10-CipherScools.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //Increment operator example
+    // int i=0;int j=0;int k=0;
+    // cout<<i<<j<<k<<endl;
+    // j=i++;
+    // cout<<i<<j<<k<<endl;
+    // k=++i;
+    // cout<<i<<j<<k<<endl;
+    //Printing number from 1 to 2000
+    for(int i=1;i<201;i++){
+        cout<<" "<<i;
+    }
+
+}
