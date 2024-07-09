4f8870baf4f581a040909ed5b6242e3d606a6b91
Nihal.k <121479386+nihalnoor01@users.noreply.github.com>
2024-07-09 23:46:47 +0530

    Add files via upload

---
+++ Lecture12-CipherSchools.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int PIN;
+    int count=1;
+    // cout<<"Please enter the password: ";
+    // cin>>PIN;
+    // while(PIN<99999){
+    //     cout<<"Your password does not meet the requirements, Please try again."<<endl;
+    //     cin>>PIN;
+
+    // }
+    // cout<<"The user has now entered a correct password."<<endl;
+    //Do Whiles:
+    // do{
+    //     cin>>PIN;
+    // }while(PIN<99999);
+    //Infinite loops:
+    // while(1){
+    //     cout<<"Nihal ";
+    //     count++;
+    //     if(count==100) break;
+    //Using for loops:
+    for(int i=1;i<100;){
+        cout<<"Nihaal ";
+        count++;
+        if(count==100){break;}
+    }
+    }
+
+       
+    
+
