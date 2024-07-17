c48fe7ac194e482696cd73334f38ba6847f2cc96
Nihal.k <121479386+nihalnoor01@users.noreply.github.com>
2024-07-18 00:26:27 +0530

    Add files via upload

---
+++ Lecture19-CipherSchools.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    long long int c;
+    cout<<"Size of an int : "<<sizeof(a)<<endl;
+    cout<<"Size of a long long int : "<<sizeof(c)<<endl;
+    return  0;
+
+}
