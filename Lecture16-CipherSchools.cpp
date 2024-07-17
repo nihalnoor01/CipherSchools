4943d152dfdea526e96567772fe94495cb70de3c
Nihal.k <121479386+nihalnoor01@users.noreply.github.com>
2024-07-17 21:59:48 +0530

    Add files via upload

---
+++ Lecture16-CipherSchools.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int sum(int, int);
+int main(){
+    int a,b;
+    a=3;b=5;
+    int c;
+    cout<<a<<" "<<b<<endl;//Values before calling the sum function
+    c=sum(a,b);
+    cout<<c<<endl;
+    cout<<a<<" "<<b<<endl;//Values after calling sum function
+    return 0;
+}
+int sum(int a,int b){
+    int c;
+    
+    c=a+b;
+    a=11;
+    b=234;
+    return c;
+}
