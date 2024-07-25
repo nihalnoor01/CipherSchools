1f310a7cab757cc77907c91851917411b8d444b6
Nihal.k <121479386+nihalnoor01@users.noreply.github.com>
2024-07-25 15:03:37 +0530

    Add files via upload

---
+++ Lecture26-CipherSchools.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//     char name[1000];
+
+//     cout << "Enter your name: (Note: Enter '#' after the end of your name)";
+
+//     int i = 0; // Initial value of i
+
+//     while (1) { // Loop until '#' is encountered
+//         cin >> name[i];
+//         if (name[i] == '#') break;
+//         i++; // Increment i to store the next character
+//     }
+
+//     i = 0; // Reset i to 0 before running the second while loop
+
+//     cout << "The name you have just entered is " << endl;
+
+//     while (name[i] != '#') { // Loop until '#' is encountered
+//         cout << name[i];
+//         i++;
+//     }
+string name;
+cin>>name;
+cout<<name;
+
+    return 0;
+}
