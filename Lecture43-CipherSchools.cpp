46ccbffc6037a21858d8042cab84364153b0f757
Nihal.k <121479386+nihalnoor01@users.noreply.github.com>
2024-07-27 19:45:50 +0530

    Create Lecture43-CipherSchools.cpp

---
+++ Lecture43-CipherSchools.cpp
@@ -0,0 +1,5 @@
+In this lecture, we learned that the worst-case scenario can be the best parameter for evaluating an algorithm's efficiency because it provides a guarantee on the maximum time or resources the algorithm will need. 
+  This helps ensure reliability and performance, even in the most demanding situations. Real-life examples, like reviews in Shopping websites,
+  illustrated why considering the worst case is crucial for robust system design.
+  We also discussed how increasing the input size "n" affects the algorithm's performance, using exponential growth as an example.
+  As "n" grows, the time or space required by an inefficient algorithm can increase dramatically, highlighting the importance of choosing efficient algorithms.
