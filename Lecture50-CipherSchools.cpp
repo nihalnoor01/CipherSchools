e55a141d28a352cf96bc1aae2176285bd7f802b8
Nihal.k <121479386+nihalnoor01@users.noreply.github.com>
2024-07-28 11:48:30 +0530

    Add files via upload

---
+++ Lecture50-CipherSchools.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    string name;
+    int mids;
+    float ends;
+    bool pre_abs;
+
+
+    string name_1="Bro";
+    int mids_1 =21;
+    float ends_1=21;
+    bool present=1;
+
+    cout<<name_1<<" "<<mids_1<<" "<<ends_1<<" "<<present<<endl;
+
+    //This way of storing the details of an object is highly impractical
+    
+}
